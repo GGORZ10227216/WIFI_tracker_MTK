d2b866c3c15db57ef19e22ba1a337f10f261e6a8
ORZGG <ab167943@gmail.com>
2016-09-19 21:10:32 +0800

    TreeWidget 改成 TreeView

---
+++ src/mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <iostream>
+#include <QTimer>
+#include <QDateTime>
+#include <QTcpSocket>
+
+using namespace std ;
+
+void MainWindow::ShowCurrentTime() {
+    QDateTime time = QDateTime::currentDateTime() ;
+    QString winTitle( "WIFI_tracker_MTK " ) ;
+    winTitle += time.toString( "yyyy-MM-dd hh:mm:ss" ) ;
+    this->setWindowTitle( winTitle );
+} // ShowCurrentTime()
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ShowCurrentTime();
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(ShowCurrentTime()));
+    timer->start(1000); //time specified in ms
+
+    cout << "hello world" << endl ;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
